706417f113cbbf839dc05286a22b1013d02d0482
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2020-05-06 17:54:46 -0500

    Fixing Bio and syntax

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("scroll", function(){
+            const parallax = document.querySelector('.parallax');
+            let scrollPos = window.pageYOffset;
+            parallax.style.transform ='translateY(' + scrollPos * .5 + 'px)';
+});
+
+// $(document).on("click","#proj1" ,showModal1);
+// $(document).on("click","#proj2" ,showModal2);
+// $(document).on("click","#close" ,hideModal);
+
+// function showModal1(e){
+// 	$('#modal1').addClass('showModal');
+// }
+// function showModal2(e){
+// 	$('#modal2').addClass('showModal');
+// }
+
+// function hideModal(e){
+// 	$('.modal').removeClass('showModal');
+// }
+
+
+// $(document).click(function (e) {
+//     if ($(e.target).is('.modal')) {
+//         hideModal();
+//     }
+// });
